b2a42d9240ab5636a36d17a9b90028b93b782eeb
Ho-mmd <ws000617@gmail.com>
2022-02-25 18:06:45 +0900

    동적할당...

--- C/C/B3.c
+++ C/C/B3.c
@@ -1,6 +1,7 @@
 # define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+
 int main(void) {
 	
 }
@@ -53,3 +54,95 @@ void BOJ2439() {
 	}
 	return 0;
 }
+
+//https://www.acmicpc.net/problem/2742
+void BOJ2742() {
+	int a;
+	scanf("%d", &a);
+	for (int i = a; i > 0; i--) {
+		printf("%d\n", i);
+	}
+	return 0;
+}
+
+//https://www.acmicpc.net/problem/10950
+#include <stdlib.h>
+void BOJ10950() {
+	int a, flag = 0, sum = 0;
+	int* arr;
+
+	scanf("%d", &a);
+
+	arr = (int*)malloc(sizeof(int) * a * 3);
+
+	for (int i = 0; i < a * 2; i++) {
+		scanf("%d", &arr[i]);
+	}
+	for (int i = 0; i < a * 2; i++) {
+		sum += arr[i];
+		if (flag == 1) {
+			printf("%d\n", sum);
+			flag = 0;
+			sum = 0;
+		}
+		else
+			flag++;
+	}
+	free(arr);
+	return 0;
+}
+
+//https://www.acmicpc.net/problem/10871
+#include <stdlib.h>
+void BOJ10871() {
+	int a, b, flag = 0;;
+	scanf("%d %d", &a, &b);
+
+	int* arr = (int*)malloc(sizeof(int) * a * 2);
+	int* arr1 = (int*)malloc(sizeof(int) * a * 2);
+
+	for (int i = 0; i < a; i++) {
+		scanf("%d", &arr[i]);
+		if (arr[i] < b) {
+			arr1[flag] = arr[i];
+			flag++;
+		}
+	}
+	for (int i = 0; i < flag; i++) {
+		printf("%d ", arr1[i]);
+	}
+	free(arr);
+	free(arr1);
+
+	return 0;
+}
+
+//https://www.acmicpc.net/problem/2884
+void BOJ2884() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	if (b >= 45)
+		printf("%d %d", a, b - 45);
+	else {
+		if (a == 0)
+			printf("%d %d", 23, b + 15);
+		else
+			printf("%d %d", a - 1, b + 15);
+	}
+
+	return 0;
+}
+
+//https://www.acmicpc.net/problem/10952
+void BOJ10952() {
+	int a, b;
+	while (1) {
+		scanf("%d %d", &a, &b);
+		if (a == 0 && b == 0)
+			break;
+		printf("%d\n", a + b);
+	}
+	return 0;
+}
+
+
