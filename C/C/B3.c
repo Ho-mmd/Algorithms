65874d5491d8b9df5dc23bb2cdd0cea8944a59cd
Ho-mmd <ws000617@gmail.com>
2022-03-13 11:15:20 +0900

    Be simple

--- C/C/B3.c
+++ C/C/B3.c
@@ -893,4 +893,88 @@ void BOJ2501() {
 	return 0;
 }
 
+//https://www.acmicpc.net/problem/2875
+void BOJ2875() {
+	//내 답안
+	/*int N, M, K, Ntmp, Ntemp, a, b, c;
+	scanf("%d %d %d", &N, &M, &K);
+	Ntmp = N / 2;
+	Ntemp = N % 2;
+	if (K == 0)
+		printf("%d", M > Ntmp ? Ntmp : M);
+	else if (Ntmp > M) {
+		c = 2 * (Ntmp - M) - K;
+		if (c >= 0)
+			printf("%d", M);
+		else {
+			c *= -1;
+			a = c / 3;
+			b = c % 3;
+			M -= a;
+			if (b == 1 && Ntemp == 1 || b == 0)
+				printf("%d", M);
+			else
+				printf("%d", M - 1);
+		}
+	}
+	else {
+		c = M - Ntmp - K;
+		if (c >= 0)
+			printf("%d", Ntmp);
+		else {
+			c *= -1;
+			a = c / 3;
+			b = c % 3;
+			Ntmp -= a;
+			if (b == 1 && Ntemp == 1 || b == 0)
+				printf("%d", Ntmp);
+			else
+				printf("%d", Ntmp - 1);
+		}
+	}
+	return 0;*/
+
+	//정석답안
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	while (c) {
+		if (a >= 2 * b)
+			a--;
+		else
+			b--;
+		c--;
+	}
+	printf("%d", a > b ? b : a);
+
+	return 0;
+}
+
+//https://www.acmicpc.net/problem/10824
+void BOJ10824() {
+	long long a, b, c, d, temp, cnt = 1, i;
+	scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+	temp = b;
+	while (temp / 10 != 0) {
+		temp /= 10;
+		cnt++;
+	}
+	for (i = 0; i < cnt; i++) {
+		a *= 10;
+	}
+	a += b;
+	cnt = 1;
+	temp = d;
+	while (temp / 10 != 0) {
+		temp /= 10;
+		cnt++;
+	}
+	for (i = 0; i < cnt; i++) {
+		c *= 10;
+	}
+	c += d;
+
+	printf("%lld", a + c);
+
+	return 0;
+}
 
