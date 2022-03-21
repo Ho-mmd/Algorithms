849bed122eca65bb3c95ba29a3cba968ea09572a
Ho-mmd <ws000617@gmail.com>
2022-03-21 22:49:08 +0900

    time attack

--- C/C/B3.c
+++ C/C/B3.c
@@ -5,6 +5,7 @@
 int main(void) {
 	
 }
+	
 
 //https://www.acmicpc.net/problem/2739
 void BOJ2739() {
@@ -1036,3 +1037,30 @@ void BOJ10833() {
 	return 0;
 }
 
+//https://www.acmicpc.net/problem/2506
+#include <stdlib.h>
+void BOJ2506() {
+	int n, i, cnt = 0, sum = 0;
+
+	scanf("%d", &n);
+
+	int* num = (int*)malloc(sizeof(int) * n);
+
+	for (i = 0; i < n; i++) {
+		scanf("%d", &num[i]);
+
+		if (num[i] == 1) {
+			sum++;
+			if (num[i - 1] == 1) {
+				cnt++;
+				sum += cnt;
+			}
+			else
+				cnt = 0;
+		}
+	}
+	free(num);
+	printf("%d", sum);
+
+	return 0;
+}
