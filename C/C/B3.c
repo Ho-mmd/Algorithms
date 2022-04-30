5bed38968cc5d43ce0906ea6ea47390bb8313f45
Ho-mmd <ws000617@gmail.com>
2022-04-30 19:32:10 +0900

    restart

--- C/C/B3.c
+++ C/C/B3.c
@@ -1243,3 +1243,27 @@ void BOJ15953() {
 	}
 	return 0;
 }
+
+//https://www.acmicpc.net/problem/3460
+void BOJ3460() {
+	int a, n, b[100] = { 0 }, c;
+	scanf("%d", &a);
+	for (int i = 0; i < a; i++) {
+		scanf("%d", &n);
+		c = 0;
+		while (n >= 2) {
+			b[c] = n % 2;
+			n /= 2;
+			c++;
+		}
+		b[c] = n;
+
+		for (int i = 0; i <= c; i++) {
+			if (b[i] == 1)
+				printf("%d ", i);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
