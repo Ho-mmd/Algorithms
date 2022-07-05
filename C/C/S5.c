5ac6b1c8e6fd3d1e4f75c4373cdbadde5d11b669
Ho-mmd <ws000617@gmail.com>
2022-07-05 09:41:15 +0900

    merge sort

--- C/C/S5.c
+++ C/C/S5.c
@@ -1,65 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <stdlib.h>
-
-void Quicksort(int arr[], int start, int end) {
-	int index;
-
-	if (start < end) {
-		index = Partition(arr, start, end);
-		Quicksort(arr, start, index - 1);
-		Quicksort(arr, index + 1, end);
-	}
-	else
-		return;
-}
-
-int Partition(int arr[], int start, int end) {
-	int tool = (start + end) / 2;
-	int temp;
-
-	temp = arr[tool];
-	arr[tool] = arr[end];
-	arr[end] = temp;
-
-	int pivot = arr[end];
-	int i, index = start;
-
-	for (i = start; i < end; i++) {
-		if (arr[i] <= pivot) {
-			temp = arr[i];
-			arr[i] = arr[index];
-			arr[index] = temp;
-			index++;
-		}
-	}
-
-	temp = arr[index];
-	arr[index] = arr[end];
-	arr[end] = temp;
-
-	return index;
-}
 
 int main(void) {
-	int n, i;
-
-	scanf("%d", &n);
-
-	int* arr = malloc(sizeof(int) * n);
-
-	for (i = 0; i < n; i++) 
-		scanf("%d", &arr[i]);
 	
-	Quicksort(arr, 0, n - 1);
-
-	for (i = 0; i < n; i++)
-		printf("%d ", arr[i]);
-
-	free(arr);
-
-	return 0;
 }
 
 
@@ -174,5 +118,73 @@ void BOJ2941() {
 	return 0;
 }
 
+//https://www.acmicpc.net/problem/2941
+#include <stdlib.h>
+void merge(int arr[], int start, int mid, int end) {
+	int b[1000001];
+	int i = start, j = mid + 1, k = 0;
 
+	while (i <= mid && j <= end) {
+		if (arr[i] <= arr[j]) {
+			b[k] = arr[i];
+			k++;
+			i++;
+		}
+		else {
+			b[k] = arr[j];
+			j++;
+			k++;
+		}
+	}
+
+	while (i <= mid) {
+		b[k] = arr[i];
+		i++;
+		k++;
+	}
+
+	while (j <= end) {
+		b[k] = arr[j];
+		j++;
+		k++;
+	}
+
+	k--;
+
+	while (k >= 0) {
+		arr[start + k] = b[k];
+		k--;
+	}
+}
+void mergeSort(int arr[], int start, int end) {
+	if (start < end) {
+		int mid = (start + end) / 2;
+
+		mergeSort(arr, start, mid);
+		mergeSort(arr, mid + 1, end);
+
+		merge(arr, start, mid, end);
+	}
+	else
+		return;
+}
+void BOJ2751() {
+	int n, i;
+
+	scanf("%d", &n);
+
+	int* arr = malloc(sizeof(int) * n);
+
+	for (i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+
+	mergeSort(arr, 0, n - 1);
+
+	for (i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+
+	free(arr);
+
+	return 0;
+}
 
