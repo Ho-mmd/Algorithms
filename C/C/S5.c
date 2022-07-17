4e70a3a5b3a43cf677791b617b17f6c08204445d
Ho-mmd <ws000617@gmail.com>
2022-07-17 19:33:45 +0900

    7/17

--- C/C/S5.c
+++ C/C/S5.c
@@ -1,9 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-	
+	char *arr[50];
+
+	scanf("%s", &arr[0]);
+
+	printf("%s", *arr);
+
+	return 0;
 }
 
 
@@ -206,6 +213,45 @@ void BOJ2869() {
 	}
 
 	printf("%d", day);
+
+	return 0;
+}
+
+//https://www.acmicpc.net/problem/7568
+void BOJ7568() {
+	int n, i, j, cnt;
+
+	scanf("%d", &n);
+
+	int* he = malloc(sizeof(int) * n);
+	int* we = malloc(sizeof(int) * n);
+	int* con = malloc(sizeof(int) * n);
+
+	for (i = 0; i < n; i++) {
+		scanf("%d", &he[i]);
+		scanf("%d", &we[i]);
+		con[i] = 0;
+	}
+
+	for (i = 0; i < n; i++) {
+		cnt = 1;
+
+		for (j = 0; j < n; j++) {
+			if (he[i] < he[j]) {
+				if (we[i] < we[j])
+					cnt++;
+			}
+		}
+
+		con[i] = cnt;
+	}
+
+	for (i = 0; i < n; i++)
+		printf("%d ", con[i]);
+
+	free(he);
+	free(we);
+	free(con);
 
 	return 0;
 }
