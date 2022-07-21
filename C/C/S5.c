ba3037df3b12d62f8f56ee22913dad9a0e1bd23f
Ho-mmd <ws000617@gmail.com>
2022-07-21 21:06:19 +0900

    7/21

--- C/C/S5.c
+++ C/C/S5.c
@@ -457,3 +457,99 @@ void BOJ11650() {
 	return 0;
 }
 
+//https://www.acmicpc.net/problem/10814
+#include <stdlib.h>
+typedef struct {
+	char name[101];
+	int age;
+	int sqe;
+}Person;
+void Quicksort(Person arr[], int start, int end) {
+	int index;
+
+	if (start < end) {
+		index = Partition(arr, start, end);
+		Quicksort(arr, start, index - 1);
+		Quicksort(arr, index + 1, end);
+	}
+	else
+		return;
+}
+int Partition(Person arr[], int start, int end) {
+	Person pivot = arr[end];
+	int index = start, i;
+	Person tmp;
+
+	for (i = start; i < end; i++) {
+		if (arr[i].age < pivot.age) {
+			tmp = arr[i];
+			arr[i] = arr[index];
+			arr[index] = tmp;
+			index++;
+		}
+		else if (arr[i].age == pivot.age) {
+			if (arr[i].sqe < pivot.sqe) {
+				tmp = arr[i];
+				arr[i] = arr[index];
+				arr[index] = tmp;
+				index++;
+			}
+		}
+	}
+
+	tmp = arr[index];
+	arr[index] = arr[end];
+	arr[end] = tmp;
+
+	return index;
+}
+void BOJ10814() {
+	int n;
+
+	scanf("%d", &n);
+
+	Person* arr = (Person*)malloc(sizeof(Person) * n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d %s", &arr[i].age, &arr[i].name);
+		arr[i].sqe = i;
+	}
+
+	Quicksort(arr, 0, n - 1);
+
+	for (int i = 0; i < n; i++)
+		printf("%d %s\n", arr[i].age, arr[i].name);
+
+	free(arr);
+
+	return 0;
+}
+
+//https://www.acmicpc.net/problem/1436
+#include <string.h>
+void BOJ1436() {
+	int n, i = 666, cnt = 0;
+	char cmp[4] = "666";
+	char tmp[101];
+
+	scanf("%d", &n);
+
+	while (1) {
+		if (cnt == n)
+			break;
+
+		sprintf(tmp, "%d", i);
+
+		if (strstr(tmp, cmp) != NULL) {
+			cnt++;
+		}
+
+		i++;
+	}
+
+	i--;
+
+	printf("%d", i);
+
+	return 0;
+}
