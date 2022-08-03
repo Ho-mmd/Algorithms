559771f181c9f765744403a5911f5ed116d98b0f
Ho-mmd <ws000617@gmail.com>
2022-08-03 23:39:01 +0900

    8/03

--- C/C/S5.c
+++ C/C/S5.c
@@ -967,3 +967,35 @@ void BOJ17478() {
 	return 0;
 }
 
+//https://www.acmicpc.net/problem/11004
+#include <stdlib.h>
+int compare1(const void* a, const void* b) {
+	int num1 = *(int*)a;
+	int num2 = *(int*)b;
+
+	if (num1 > num2)
+		return 1;
+	else if (num1 < num2)
+		return -1;
+	else
+		return 0;
+}
+void BOJ11004() {
+	int n, a;
+
+	scanf("%d%d", &n, &a);
+
+	int* arr = (int*)malloc(sizeof(int) * n);
+
+	for (int i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+
+	qsort(arr, n, sizeof(int), compare1);
+
+	printf("%d", arr[a - 1]);
+
+	free(arr);
+
+	return 0;
+}
+
