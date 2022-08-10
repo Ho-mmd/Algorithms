883930ef6bf0d91bec39ee26b9eb034f67d350e2
Ho-mmd <ws000617@gmail.com>
2022-08-10 13:28:03 +0900

    8.10

--- C/C/S5.c
+++ C/C/S5.c
@@ -1126,3 +1126,24 @@ void BOJ2740() {
 
 	return 0;
 }
+
+//https://www.acmicpc.net/problem/10867
+void BOJ10867() {
+	int n, num;
+	int arr[2001] = { 0 };
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &num);
+		arr[num + 1000] = 1;
+	}
+
+	for (int j = 0; j < 2001; j++) {
+		if (arr[j] == 1)
+			printf("%d ", j - 1000);
+	}
+
+	return 0;
+}
+
