44c63bb02d73d40f30e57415d1ba1369db023d96
Ho-mmd <ws000617@gmail.com>
2022-08-29 15:03:03 +0900

    8/29

--- C/C/S4.c
+++ C/C/S4.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-
+	
 }
 
 //https://www.acmicpc.net/problem/1051
@@ -33,6 +33,52 @@ void BOJ1051() {
 	}
 
 	printf("%d", ans * ans);
+
+	return 0;
+}
+
+//https://www.acmicpc.net/problem/1026
+#include <stdlib.h>
+int compare0(const void* a, const void* b) {
+	int num1 = *(int*)a;
+	int num2 = *(int*)b;
+
+	if (num1 > num2)
+		return 1;
+	else if (num1 < num2)
+		return -1;
+	else
+		return 0;
+}
+int compare1(const void* a, const void* b) {
+	int num1 = *(int*)a;
+	int num2 = *(int*)b;
+
+	if (num1 > num2)
+		return -1;
+	else if (num1 < num2)
+		return 1;
+	else
+		return 0;
+}
+void BOJ1026() {
+	int arr1[51] = { 0 }, arr2[51] = { 0 };
+	int i, n, sum = 0;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+		scanf("%d", &arr1[i]);
+	for (i = 0; i < n; i++)
+		scanf("%d", &arr2[i]);
+
+	qsort(arr1, n, sizeof(int), compare0);
+	qsort(arr2, n, sizeof(int), compare1);
+
+	for (i = 0; i < n; i++)
+		sum += arr1[i] * arr2[i];
+
+	printf("%d", sum);
 
 	return 0;
 }
