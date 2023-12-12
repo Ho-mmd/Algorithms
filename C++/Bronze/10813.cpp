1ea9f7c7cc765ef0aaa34ca739a3185380eb0a7a
ho <ws000617@gmail.com>
2023-12-12 19:38:13 +0100

    12_Dec

---
+++ C++/Bronze/10813.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    int N, M, i, j;
+
+    std::cin >> N >> M;
+
+    int* arr = new int[N];
+
+    for(int k = 0; k < N; k++)
+        arr[k] = k + 1;
+
+    for(int k = 0; k < M; k++) {
+        std::cin >> i >> j;
+        std::swap(arr[i - 1], arr[j - 1]);
+    }
+
+    for(int k = 0; k < N; k++)
+        std::cout << arr[k] << " ";
+    
+    delete[] arr;
+
+    return 0;
+}
