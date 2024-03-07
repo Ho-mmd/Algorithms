e86fe0fb130dad83073e349bd180eced2812cb1a
ho <ws000617@gmail.com>
2024-03-07 13:50:10 +0100

    Brute Force (4/6)

---
+++ BOJ/C++/Bronze/19532.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+		int a, b, c, d, e, f, x, y;
+
+		std::cin >> a >> b >> c >> d >> e >> f;
+
+		for(int i = -999; i <= 999; i++) {
+				for(int j = -999; j <= 999; j++) {
+						if(a * i + b * j == c && d * i + e * j == f) {
+								std::cout << i << " " << j;
+								return 0;
+						}
+				}
+		}
+}
