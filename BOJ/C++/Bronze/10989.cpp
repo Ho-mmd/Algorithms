a1474fa17333e0bc27f9536b462e7adf9ec89b39
ho <ws000617@gmail.com>
2024-03-16 01:05:15 +0100

    Sort (10/11)

--- BOJ/C++/Bronze/10989.cpp
+++ BOJ/C++/Bronze/10989.cpp
@@ -1,46 +1,25 @@
 #include <iostream>
-#include <vector>
 
 int main() {
-		std::vector<int> num;
-		int N, k, max = 0;
+
+		int cnt[10000] = { 0 };
+		int N, k;
+
+		std::ios_base::sync_with_stdio(false);
+		std::cin.tie(NULL);
 
 		std::cin >> N;
 
 		for(int i = 0; i < N; i++) {
 				std::cin >> k;
-				num.push_back(k);
-				
-				if(max < k)
-						max = k;
-		}
-		
-		int *cnt = new int[max + 1];
-		int *ans = new int[N];
-
-
-		for(int i = 0; i < max + 1; i++) {
-				cnt[i] = 0;
-		}
-
-		for(int i = 0; i < N; i++) 
-				cnt[num[i] - 1]++;
-			
-		for(int i = 1; i < max + 1; i++)
-				cnt[i] = cnt[i - 1] + cnt[i];
-
-		for(int i = N - 1; i >= 0; i--) {
-				ans[cnt[num[i] - 1]] = num[i];
-				cnt[num[i] - 1] -= 1;
-		}
-
-
-		for(int i = 0; i < N; i++)
-				std::cout << ans[i] << "\n";
+				cnt[k - 1]++;
+		}	
+						
+		for(int i = 0; i < 10000; i++)
+				if(cnt[i] != 0)
+						for(int j = 0; j < cnt[i]; j++)
+								std::cout << i + 1 << "\n";
 	
-		delete[] cnt;
-		delete[] ans;
-
 		return 0;
 		
 }
