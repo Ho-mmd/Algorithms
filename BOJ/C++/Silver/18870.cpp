3aa46140d1d62b4150abe3d1c79ea6ef498a8323
ho <ws000617@gmail.com>
2024-03-16 19:51:54 +0100

    Sort (11/11)

---
+++ BOJ/C++/Silver/18870.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+
+int main() {
+		
+		int N, seq = 0;
+		std::map<int, int> ans;
+
+		std::ios_base::sync_with_stdio(false);
+		std::cin.tie(NULL);
+
+		std::cin >> N;
+
+		int *tmp = new int[N];
+
+		for(int i = 0; i < N; i++) {
+				std::cin >> tmp[i];
+				ans.insert(std::make_pair(tmp[i], i));
+		}
+
+		for(auto iter = ans.begin(); iter != ans.end(); iter++) {
+				iter->second  = seq;
+				seq++;
+		}
+
+		for(int i = 0; i < N; i++)
+				std::cout << ans.find(tmp[i])->second << " ";
+
+		return 0;
+}
