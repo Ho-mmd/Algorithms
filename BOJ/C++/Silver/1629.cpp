f5ed0e378829c591e650b79cc4b2ed877ce394e6
ho <ws000617@gmail.com>
2024-07-22 18:56:25 +0900

    Divide and Conquer (5/9)

---
+++ BOJ/C++/Silver/1629.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+long long A, B, C, mod;
+
+long long Multi(long long b) {
+		if(b == 0)
+				return 1;
+		else if(b == 1)
+				return A % C;
+
+		mod = Multi(b / 2) % C;
+
+		if(b % 2 == 0)
+				return mod * mod % C;
+		else
+				return mod * mod % C * A % C;
+}
+
+int main() {
+		std::ios_base::sync_with_stdio(false);
+		std::cin.tie(NULL);
+
+		std::cin >> A >> B >> C;
+
+		std::cout << Multi(B);
+
+		return 0;
+}
